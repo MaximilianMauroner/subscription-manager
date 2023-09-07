2e6a78f12fd130318d5d042c4668ab243e7810c4
Maximilian Mauroner <github@relay.mauroner.eu>
2023-09-07 11:54:50 +0200

    added price history

--- src/server/db.ts
+++ src/server/db.ts
@@ -11,6 +11,33 @@ export const prisma =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  }).$extends({
+    query: {
+      subscription: {
+        async update({ model, operation, args, query }) {
+          if (args.data.price) {
+            const oldSubscrption =
+              await globalForPrisma.prisma?.subscription.findUnique({
+                where: {
+                  id: args.where.id,
+                },
+                select: {
+                  price: true,
+                },
+              });
+            if (!oldSubscrption?.price || !args.where.id) return query(args);
+            await globalForPrisma.prisma?.subscriptionPriceHistory.create({
+              data: {
+                subscriptionId: args.where.id,
+                newPrice: +args.data.price,
+                oldPrice: oldSubscrption?.price,
+              },
+            });
+          }
+          return query(args);
+        },
+      },
+    },
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
